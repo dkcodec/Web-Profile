25dac2891d68c52911c7c5e6faf25c3fbe08e84e
DKod <74229630+dkcodec@users.noreply.github.com>
2023-09-10 18:45:42 +0600

    Add files
    Header, main, navigation, home_section

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+    :header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
