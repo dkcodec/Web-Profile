795b78790efe8930cfc3d043396cc0b5693f6847
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-10-08 15:39:04 +0500

    feat: clock (logged) + assignment4

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
 //nav
 const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
 }
 
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
 }
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
 }
-
 
 //menu
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach((n) => n.addEventListener('click', linkAction))
 
 const shadowHeader = () => {
-    const header = document.getElementById('header');
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-    :header.classList.remove('shadow-header')
+  const header = document.getElementById('header')
+  this.scrollY >= 50
+    ? header.classList.add('shadow-header')
+    : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
 
@@ -44,51 +43,77 @@ window.addEventListener('scroll', shadowHeader)
 const contactForm = document.getElementById('contact-form')
 const contactMessage = document.getElementById('contact-message')
 
-const sendEmail = (e) =>{
-    e.preventDefault()
+const sendEmail = (e) => {
+  e.preventDefault()
 
-    emailjs.sendForm('service_fmdratb','template_nebytvv','#contact-form','2-Moy10DbM8Vszz3J')
-    .then(() => {
-        contactMessage.textContent = 'Message sent seccessfully ✅' 
-        setTimeout(()  =>{  
-            contactMessage.textContent = ''
+  emailjs
+    .sendForm(
+      'service_fmdratb',
+      'template_nebytvv',
+      '#contact-form',
+      '2-Moy10DbM8Vszz3J'
+    )
+    .then(
+      () => {
+        contactMessage.textContent = 'Message sent seccessfully ✅'
+        setTimeout(() => {
+          contactMessage.textContent = ''
         }, 5000)
 
         contactForm.reset()
-    }, () => {
+      },
+      () => {
         contactMessage.textContent = 'Message not sent (services error)❌'
-    })
+      }
+    )
 }
 
 contactForm.addEventListener('submit', sendEmail)
 
+// Current date
+const dateFunction = () => {
+  const clock = document.getElementById('clock')
+  console.log(clock)
+  const date = new Date()
+  const hour = String(date.getHours()).padStart(2, 0)
+  const minutes = String(date.getMinutes()).padStart(2, 0)
+  const seconds = String(date.getSeconds()).padStart(2, 0)
+  clock.textContent = `${hour}:${minutes}:${seconds}`
+
+  setInterval(dateFunction, 1000)
+  dateFunction()
+}
+// window.addEventListener('load', dateFunction)
+
 //scroll
 
-const scrollUp = () =>{
-    const scrollUp = document.getElementById('scroll-up')
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
 
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                        : scrollUp.classList.remove('show-scroll')
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 const sections = document.querySelectorAll('section[id]')
-const scrollActive = ()=>{
-    const scrollDown = window.scrollY
+const scrollActive = () => {
+  const scrollDown = window.scrollY
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 58
-        const sectionId = current.getAttribute('id')
-        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 58
+    const sectionId = current.getAttribute('id')
+    const sectionsClass = document.querySelector(
+      '.nav__menu a[href*=' + sectionId + ']'
+    )
 
-        if(scrollDown > sectionTop && scrollDown <= sectionHeight + sectionTop){
-            sectionsClass.classList.add('active__link')
-        }
-        else{
-            sectionsClass.classList.remove('active__link')    
-        }
-    })
+    if (scrollDown > sectionTop && scrollDown <= sectionHeight + sectionTop) {
+      sectionsClass.classList.add('active__link')
+    } else {
+      sectionsClass.classList.remove('active__link')
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive)
 
@@ -100,30 +125,41 @@ const iconTheme = 'ri-contrast-2-fill'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-contrast-2-line' : 'ri-contrast-2-fill'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme)
+    ? 'ri-contrast-2-line'
+    : 'ri-contrast-2-fill'
 
-if(selectedTheme){
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'ri-contrast-2-line' ? 'add' : 'remove'](iconTheme)
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  )
+  themeButton.classList[
+    selectedIcon === 'ri-contrast-2-line' ? 'add' : 'remove'
+  ](iconTheme)
 }
 
 themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
 
-    localStorage.setItem('selectes-theme', getCurrentTheme())
-    localStorage.setItem('selectes-icon', getCurrentIcon())
+  localStorage.setItem('selectes-theme', getCurrentTheme())
+  localStorage.setItem('selectes-icon', getCurrentIcon())
 })
 
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duraction: 2500,
-    delay: 400,
-    reset: true
+  origin: 'top',
+  distance: '60px',
+  duraction: 2500,
+  delay: 400,
+  reset: true,
 })
 
-sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
-sr.reveal(`.home__name, home.info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin: 'left'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
+sr.reveal(
+  `.home__name, home.info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`,
+  { origin: 'left' }
+)
+sr.reveal(`.services__card, .projects__card`, { interval: 100 })
